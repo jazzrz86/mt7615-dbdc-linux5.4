86f3268b33d83922d7ef5fac9b85f257cf23321c
Your Name <you@example.com>
2020-11-10 15:38:45 +1100

    add link to whnat

--- drivers/mt_wifi/files/82_load_wifi.sh
+++ drivers/mt_wifi/files/82_load_wifi.sh
@@ -5,6 +5,7 @@ load_wifi() {
 	[ -e "/lib/modules/$kernel_version/mt7603e.ko" ] && modprobe mt7603e
 	[ -e "/lib/modules/$kernel_version/mt76x2_ap.ko" ] && modprobe mt76x2_ap
 	[ -e "/lib/modules/$kernel_version/mt_wifi.ko" ] && modprobe mt_wifi
+    [ -e "/lib/modules/$kernel_version/mt_whnat.ko" ] && modprobe mt_whnat
 }
 
 boot_hook_add preinit_main load_wifi
